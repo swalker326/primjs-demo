610b8854b35bb2db65e5319b1cee039c397d0cff
shane <shane@swalker.dev>
2025-08-06 17:50:49 -0500

    initial commit

---
+++ rspack.config.ts
@@ -0,0 +1,66 @@
+import { withZephyr } from "zephyr-rspack-plugin";
+import { rspack } from "@rspack/core";
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ["last 2 versions", "> 0.2%",  "not dead",  "Firefox ESR"];
+
+export default withZephyr()({
+	entry: {
+		main: "./src/index.ts"
+	},
+	resolve: {
+		extensions: ["...", ".ts"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: "asset"
+			},
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "ecmascript"
+								}
+							},
+							env: { targets }
+						}
+					}
+				]
+			},
+			{
+				test: /\.ts$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							jsc: {
+								parser: {
+									syntax: "typescript"
+								}
+							},
+							env: { targets }
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [new rspack.HtmlRspackPlugin({ template: "./index.html" })],
+	optimization: {
+		minimizer: [
+			new rspack.SwcJsMinimizerRspackPlugin(),
+			new rspack.LightningCssMinimizerRspackPlugin({
+				minimizerOptions: { targets }
+			})
+		]
+	},
+	experiments: {
+		css: true
+	}
+});
